a6521eee2a51f80e33fece38a471c7e1421f3fb1
James J Balamuta <james.balamuta@gmail.com>
2017-11-30 14:54:15 -0600

    Add pi_reference

---
+++ R/pi_reference.R
@@ -0,0 +1,16 @@
+
+#' Mapping of Entries of Pi to Latent Attribute Classes
+#'
+#' Obtains mapping of entries of pi to latent attribute classes.
+#' @param K A `numeric`, denoting the number of attributes.
+#' @return A `matrix`, where row c represents the latent class
+#' corresponding to entry c of pi
+#' @author Steven Andrew Culpepper, Aaron Hudson
+#' @export
+pi_reference = function(K) {
+  biject.vector = bijectionvector(K)
+  As = as.matrix(expand.grid(rep(list(c(0,1)), K)))
+  a = As%*%biject.vector
+  As = As[a+1,]
+  return(As)
+}
