e84f6d6aca262f5ff9b1bffa59b8d82c1e49b36a
James Balamuta <balamut2@illinois.edu>
2018-12-04 16:48:49 -0600

    Improve rrum documentation

--- man-roxygen/rrum-example.R
+++ man-roxygen/rrum-example.R
@@ -1,32 +1,36 @@
 #' @examples 
-#' \dontrun{
-#' ###################################
-#' ### Generate random data from rRUM
-#' ### and recover parameters with
-#' ### Gibbs sampler
-#' ###################################
+#' # Set seed for reproducibility
 #' set.seed(217)
+#' 
 #' # Define Simulation Parameters
-#' N <- 1000 # number of individuals
-#' J <- 6 # number of items
-#' K <- 2 # number of attributes
-#' As <- pi_reference(K) # matrix where rows represent attribute classes
-#' pis <- c(.1, .2, .3, .4) # pi
-#' Q <- rbind(c(1,0),
-#'            c(0,1),
-#'            c(1,0),
-#'            c(0,1),
-#'            c(1,1),
-#'            c(1,1))
-#' pistar <- rep(.9, J) # pistar
-#' rstar <- .5 * Q # rstar
-#' alpha <- As[sample(1:(K ^ 2), N, replace = TRUE, pis),]
+#' N = 1000 # number of individuals
+#' J = 6 # number of items
+#' K = 2 # number of attributes
+#' 
+#' # Matrix where rows represent attribute classes
+#' As = sim_alpha_matrix(K) 
+#' 
+#' # Latent Class probabilities
+#' pis = c(.1, .2, .3, .4) 
+#' 
+#' # Q Matrix
+#' Q = rbind(c(1, 0),
+#'           c(0, 1),
+#'           c(1, 0),
+#'           c(0, 1),
+#'           c(1, 1),
+#'           c(1, 1)
+#'     )
+#'     
+#' # The probabiliies of answering each item correctly for individuals 
+#' # who do not lack any required attribute
+#' pistar = rep(.9, J)
+#' 
+#' # Penalties for failing to have each of the required attributes
+#' rstar  = .5 * Q
+#' 
+#' # Randomized alpha profiles
+#' alpha  = As[sample(1:(K ^ 2), N, replace = TRUE, pis),]
+#' 
 #' # Simulate data
-#' Y <- sim_rrum(Q, rstar, pistar, alpha)
-#' # Recover simulation parameters with Gibbs Sampler
-#' Gibbs.out <- rrum(Y, Q)
-#' burnin <- 1:5000 # iterations to be discarded from chain as burnin
-#' rstar.mean <- with(Gibbs.out, apply(RSTAR[,,-burnin], c(1, 2), mean))
-#' pistar.mean <- with(Gibbs.out, apply(PISTAR[,-burnin], 1, mean))
-#' pis.mean <- with(Gibbs.out, apply(PI[,-burnin], 1 ,mean))
-#' }
+#' rrum_items = sim_rrum_items(Q, rstar, pistar, alpha)
